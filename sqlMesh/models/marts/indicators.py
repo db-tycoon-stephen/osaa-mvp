06d6c440bf9dcf398c5e6e97344215c7f63cafd8
stuffbyyuki <kake.19940627@gmail.com>
2024-12-19 14:27:40 -0700

    return a sql query than a df

--- sqlMesh/models/marts/indicators.py
+++ sqlMesh/models/marts/indicators.py
@@ -1,11 +1,12 @@
 import ibis 
-from sqlmesh import ExecutionContext, model
-import typing as t
-from datetime import datetime
+from ibis.expr.operations import Namespace, UnboundTable
+from sqlmesh.core.macros import MacroEvaluator
+from sqlmesh.core.model import model
 
 
 @model(
     "marts.indicators",
+    is_sql=True,
     kind="FULL",
     columns={
         "indicator_id": "TEXT", 
@@ -18,24 +19,35 @@ from datetime import datetime
         "source": "TEXT"
     }
 )
-def execute(
-    context: ExecutionContext,
-    start: datetime,
-    end: datetime,
-    execution_time: datetime,
-    **kwargs: t.Any,
-) -> str:
-    
-    int_sdg = context.table("intermediate.sdg")
-    int_opri = context.table("intermediate.opri")
-    int_sdg_t = ibis.memtable(context.fetchdf(f"SELECT * FROM {int_sdg}"))
-    int_opri_t = ibis.memtable(context.fetchdf(f"SELECT * FROM {int_opri}"))
+def entrypoint(evaluator: MacroEvaluator) -> str:
+
+    schema = {
+        "indicator_id": "String", 
+        "country_id": "String", 
+        "year": "Int64", 
+        "value": "Decimal", 
+        "magnitude": "String", 
+        "qualifier": "String",
+        "indicator_description": "String", 
+    }
+
+    int_sdg = UnboundTable(
+        name="sdg",
+        schema=schema,
+        namespace=Namespace(database="intermediate")
+    ).to_expr()
+
+    int_opri = UnboundTable(
+        name="opri",
+        schema=schema,
+        namespace=Namespace(database="intermediate")
+    ).to_expr()
    
     unioned_t = (
         ibis
         .union(
-            int_sdg_t.mutate(source=ibis.literal("sdg")),
-            int_opri_t.mutate(source=ibis.literal("opri"))
+            int_sdg.mutate(source=ibis.literal("sdg")),
+            int_opri.mutate(source=ibis.literal("opri"))
         )
         .order_by([
             'year',
@@ -44,4 +56,4 @@ def execute(
         ])
     ) 
 
-    return unioned_t.to_pandas()
+    return ibis.to_sql(unioned_t)
