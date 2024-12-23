1e095e61f7fa35e74f87e8a4024971327ee2c0c7
stuffbyyuki <kake.19940627@gmail.com>
2024-12-23 10:17:22 -0700

    modified indicators.py

--- sqlMesh/models/marts/indicators.py
+++ sqlMesh/models/marts/indicators.py
@@ -1,33 +1,38 @@
 import ibis
 from sqlmesh.core.macros import MacroEvaluator
 from sqlmesh.core.model import model
-from macros.ibis_expressions import generate_ibis_table, get_upstream_columns_and_types
-
-column_schema = {
-    "indicator_id": "String",
-    "country_id": "String",
-    "year": "Int64",
-    "value": "Decimal",
-    "magnitude": "String",
-    "qualifier": "String",
-    "indicator_description": "String",
-}
+from macros.ibis_expressions import generate_ibis_table
+from models.intermediate.sdg import column_schema as sdg_column_schema
+from models.intermediate.opri import column_schema as opri_column_schema
 
 
-@model("marts.indicators", is_sql=True, kind="FULL", columns=column_schema)
+@model(
+    "marts.indicators",
+    is_sql=True,
+    kind="FULL",
+    columns={
+        "indicator_id": "String",
+        "country_id": "String",
+        "year": "Int64",
+        "value": "Decimal",
+        "magnitude": "String",
+        "qualifier": "String",
+        "indicator_description": "String",
+    },
+)
 def entrypoint(evaluator: MacroEvaluator) -> str:
     int_sdg = generate_ibis_table(
         evaluator,
         table_name="sdg",
         schema_name="intermediate",
-        column_schema=column_schema,
+        column_schema=sdg_column_schema,
     )
 
     int_opri = generate_ibis_table(
         evaluator,
         table_name="opri",
         schema_name="intermediate",
-        column_schema=column_schema,
+        column_schema=opri_column_schema,
     )
 
     unioned_t = ibis.union(
